4eaa8fee69007cf29735464162da990fb64df22c
perfext <62498788+programm-er-games@users.noreply.github.com>
2024-01-09 02:37:35 +0300

    Update files
    Rewrite functions in db.php
    index.php used as test file

--- db.php
+++ db.php
@@ -1,15 +1,90 @@
 <?php
 $main_conn = new SQLite3("C:\\Users\\User\\Documents\\GitHub\\key-accounting\\KA.db");
 class Db {
-    
     private $db;
-    
-    function insert(string $table, string $data) {
-        $sql = "" . $table . "" . $data . "";
-        $this->db->query($sql);
+    private $arguments;
+
+    function start(string $table, ...$args) {
+        for ($i = 0; $i < count($args); $i++) {
+            $this->arguments[$i] = $args[$i];
+        }
+        // var_dump($this->arguments);
+        $sql = "CREATE TABLE IF NOT EXISTS " . $table . " (" . join(", ", $this->arguments) . ")";
+        for ($i = 0; $i < count($args); $i++) {
+            $temp = explode(" ", $this->arguments[$i]);
+            $this->arguments[$i] = $temp[0];
+        }
+        // echo $sql."\n";
+        // var_dump($this->arguments);
+        $this->db->exec($sql);
+    }
+    function insert(string $table, array $data) {
+        $sql = "INSERT INTO " . $table . " VALUES (";
+        foreach ($this->arguments as $value) {
+            $sql .= ":" . $value . ", ";
+        }
+        $sql = substr($sql, 0, -2);
+        $sql .= ")";
+        // echo $sql;
+        $sql = $this->db->prepare($sql);
+        // var_dump($data, $this->arguments);
+        for ($i = 0; $i < count($data); $i++) {
+            $sql->bindParam(":" . $this->arguments[$i], $data[$i]);
+        }
+        $sql->execute();
+    }
+    function update(string $table, array $upd, array $cond) {
+        $sql = "UPDATE " . $table . " SET ";
+        foreach ($upd as $key => $value) {
+            $sql .= $key . " = :" . $key . "_upd, ";
+        }
+        $sql = substr($sql, 0, -2);
+        $sql .= " WHERE ";
+        foreach ($cond as $key => $value) {
+            $sql .= $key . " == :" . $key . " AND ";
+        }
+        $sql = substr($sql, 0, -5);
+        // echo $sql;
+        $sql = $this->db->prepare($sql);
+        foreach ($upd as $key => $value) {
+            $sql->bindValue(":" . $key . "_upd", $value);
+        }
+        foreach ($cond as $key => $value) {
+            $sql->bindValue(":". $key, $value);
+        }
+        $sql->execute();
+    }
+    function delete(string $table, array $cond) {
+        $sql = "DELETE FROM " . $table . " WHERE ";
+        foreach ($cond as $key => $value) {
+            $sql .= $key . " == :". $key . " AND ";
+        }
+        $sql = substr($sql, 0, -5);
+        // echo $sql;
+        $sql = $this->db->prepare($sql);
+        foreach ($cond as $key => $value) {
+            $sql->bindValue(":". $key, $value);
+        }
+        $sql->execute();
+    }
+    function get(string $table, array $cond) {
+        $sql = "SELECT * FROM " . $table . " WHERE ";
+        foreach ($cond as $key => $value) {
+            $sql .= $key . " == :" . $key . " AND ";
+        }
+        $sql = substr($sql, 0, -5);
+        // echo $sql;
+        $sql = $this->db->prepare($sql);
+        foreach ($cond as $key => $value) {
+            $sql->bindValue(":" . $key, $value);
+        }
+        var_dump($sql->execute()->fetchArray(SQLITE3_ASSOC));
     }
     function __construct(string $path) { 
         $this->db = new SQLite3($path);
+    }
+    function __destruct() {
+        $this->db->close();
     }
 }
 ?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+<?php
+$datetime = new DateTime(timezone: new DateTimeZone("+0300"));
+// echo $datetime->format(DATE_RSS);
+require "db.php";
+$test = new Db("C:\Users\User\Documents\GitHub\key-accounting\KA.db");
+$test->start("records", "name TEXT NOT NULL", "surname TEXT NOT NULL", "datetime TEXT");
+$test->insert("records", array("Alex", "Popov", $datetime->format("D, d M Y H:i:s")));
+$test->update("records", ["name" => "Alex"], ["name" => "Oleg"]);
+$test->get("records", ["name" => "Alex", "surname" => "Popov"]);
+$test->delete("records", ["surname"=> "Popov"]);
+?>
+</body>
+</html>
